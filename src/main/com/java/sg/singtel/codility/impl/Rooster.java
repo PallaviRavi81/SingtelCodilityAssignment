8eb83fdd18de2f4d4cd4465e9761890bd51f8081
PallaviRavi81 <63900360+PallaviRavi81@users.noreply.github.com>
2020-04-18 02:40:15 -0700

    Codility -Question8 Solution source code commited

--- src/main/com/java/sg/singtel/codility/impl/Rooster.java
+++ src/main/com/java/sg/singtel/codility/impl/Rooster.java
@@ -1,13 +1,34 @@
 package main.com.java.sg.singtel.codility.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rooster extends Chicken {
+
+	private static final Map<String, String> langMap;
+	static {
+		langMap = new HashMap<String, String>();
+		langMap.put("English", "Cock-a-doodle-doo");
+		langMap.put("Tamil", "cocoraco");		
+		langMap.put("Japanese", "ko-ke-kok-ko-o");		
+		langMap.put("Urdu", "kuklooku");
+
+	}
 
 	public boolean fly() {
 		System.out.println("I cannot fly");
 		return false;
 	}
-	public boolean sing() {
-		System.out.println("Cock-a-doodle-doo");
+
+	public boolean sing(String language) {
+
+		if (null == language || language.isEmpty()) {
+			// default as english
+			System.out.println("Cock-a-doodle-doo");
+		} else {
+			System.out.println(langMap.get(language));
+		}
+
 		return true;
 	}
 }
